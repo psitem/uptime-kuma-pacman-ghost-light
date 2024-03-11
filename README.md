b2af2a3f37ae28d8606598d340ce6e988fd07b65
psitem <5166927+psitem@users.noreply.github.com>
2024-03-10 22:44:01 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# uptime-kuma-pacman-ghost-light
+# Pacman Ghost Light for Uptime Kuma
+
+This is the [CircuitPython](https://circuitpython.org/) code used to build a [Paladone Pac-Man Ghost Light](https://amzn.to/49Cp8EF) whose lights adjust based on the status of monitors in [Uptime Kuma](https://uptime.kuma.pet/).
+
+##
+
+#### Requirements: 
+  - CircuitPython 8.x.
+  - Compatibled board.
+  - Adafruit NeoPixel-compatible RGB LEDs (ie: [WS2812b](https://amzn.to/43dU3Vh)).
+
+#### Optional:
+  - [SSD1306-compatible display](https://amzn.to/48IWCA0).
+
+Anything that runs CircuitPython and provides W-Fi, 5v, and 2 pins of GPIO ought to work (plus 3v3 and i2c if using the display). My development setup was on an [Raspberry Pi Pico W](https://www.raspberrypi.com/products/raspberry-pi-pico/?variant=raspberry-pi-pico-w) but deployed it with an [ESP-C3-13-Kit](https://amzn.to/3wOrRMG) board — that particular ESP board seems to be unobainium today but the [ESP-C3-12F-Kit](https://amzn.to/3PgFWsz) boards are probably equivalent.
+
+#### Installation:
+
+Copy everything from the `code/` directory to the CircuitPython root.
+
+#### Configuration:
+
+`settings.toml`:
+
+* `METRICS_URL =` Complete URL to the Prometheus metrics endpoint on your Uptime Kuma instance (ie: http://something/metrics).
+* `API_TOKEN =` Your Uptime Kuma API token. Needed if authentication is enabled. See `/settings/api-keys` on your Uptime Kuma instance.
+* `CIRCUITPY_WIFI_SSID =` Wi-Fi SSID.
+* `CIRCUITPY_WIFI_PASSWORD =` Wi-Fi password.
+* `CIRCUITPY_WEB_API_PORT =` Set to use the CircuitPython Web Workflow (ie: if your board doesn't support the "CIRCUITPY" drive).
+* `CIRCUITPY_WEB_API_PASSWORD =` Password for the CircuitPython Web Workflow (optional).
+
+`code.py`:
+
+Look for the part near the top that says Configuration Section.
+
+General settings:
+* `interval_refresh =` How often to check Uptime Kuma (in seconds).
+* `interval_timeout =` How long to wait for Uptime Kuma response (in seconds).
+* `use_ntp =` [ True | False ] Set the board clock using NTP.
+
+Body LED settings:
+* `use_body_pixels =` [ True | False ]
+* `body_pixels_pin =` GPIO to use for body LEDs.
+* `body_pixels_count =` Number of body LEDs.
+* `body_pixels_brightness =` Body LED brightness, 1 = 100%.
+
+Eye LED settings:
+* `use_eye_pixels =` [ True | False ]
+* `eye_pixels_pin =` GPIO to use for eye LEDs.
+* `eye_pixels_count =` Number of eye LEDs.
+* `eye_pixels_brightness =` Eye LED brightness, 1 = 100%.
+
+LCD settings:
+* `use_display =` [ True | False ]
+* `scl_pin =` GPIO to use for i2c SCL
+* `sda_pin =` GPIO to use for i2c SDA
+
+Note: The `lib/adafruit_displayio_ssd1306.py` library has been modified at line 57 to increase the scan rate.
+
+#### Demo:
+
+<p align="center">
+<video src="https://github.com/psitem/uptime-kuma-pacman-ghost-light/blob/main/images/pacghost-light-states.mov?raw=true" width="300" />
+</p>
+
+<p align="center">
+![Pacman Ghost cycling through light states](https://github.com/psitem/uptime-kuma-pacman-ghost-light/blob/main/images/picow-test-setup.jpg.jpg?raw=true)
+</p>
---
+++ code/code.py
@@ -0,0 +1,427 @@
+import os
+import board
+
+############################ Configuration Section ############################
+
+# URL for /metrics on your Uptime Kuma instance.
+metrics_url = os.getenv("METRICS_URL")
+
+# Need to provide an Uptime Kuma API token if authentication is enabled.
+# See <Uptime Kuma URL>/settings/api-keys
+api_token = os.getenv("API_TOKEN")
+
+interval_refresh = 15
+interval_timeout=30
+
+use_display = True              # Default False. Supports an SSD1306 i2c 128x64 display.
+scl_pin = board.GP15            # My Pico W setting
+sda_pin = board.GP14            # My Pico W setting
+# scl_pin = board.IO6             # My ESP32-C3 setting
+# sda_pin = board.IO7             # My ESP32-C3 setting
+
+use_ntp = True
+
+use_body_pixels = True          # Default False
+# body_pixels_pin = board.IO8     # My ESP32-C3 setting
+body_pixels_pin = board.GP27    # My Pico W setting
+body_pixels_count = 8
+body_pixels_brightness = 0.025  # Default 1 (100%).
+
+use_eye_pixels = True           # Default False
+# eye_pixels_pin = board.IO9    # My ESP32-C3 setting
+eye_pixels_pin = board.GP28     # My Pico W setting
+eye_pixels_count = 2
+eye_pixels_brightness = 0.025   # Default 1 (100%).
+
+# loop_light_states = True        # Default False. True will infinite loop through all the light states.
+
+########################## End Configuration Section ##########################
+
+import busio
+import gc
+import io
+import re
+import rtc
+import socketpool
+import supervisor
+import sys
+import time
+import wifi
+import adafruit_requests
+import neopixel
+import bitbangio
+
+from base64 import b64encode
+
+# The RegEx where the magic happens.
+reobj=re.compile('^(?:monitor_status{.*}\s)(\d)')
+
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+ORANGE = (255, 128, 0)
+WHITE = (255,255,255)
+OFF = (0,0,0)
+
+color_NoWifi = BLUE
+color_Unreachable = ORANGE
+color_Down = RED
+color_Up = GREEN
+color_Pending = YELLOW
+
+body_pixels = None
+eye_pixels = None
+
+if ( not 'loop_light_states' in globals() ):
+    loop_light_states = False
+if ( not 'body_pixels_count' in globals() ):
+    body_pixels_count = 1
+if ( not 'body_pixels_brightness' in globals() ):
+    body_pixels_brightness = 0
+if ( not 'body_pixels_count' in globals() ):
+    body_pixels_count = 0
+if ( not 'use_body_pixels' in globals() ):
+    use_body_pixels = False
+    body_pixels = None
+if ( use_body_pixels ):
+    if ( not 'body_pixels_brightness' in globals() ):
+        body_pixels_brightness = 0.025
+    body_pixels = neopixel.NeoPixel(body_pixels_pin, body_pixels_count, brightness=body_pixels_brightness, auto_write=True)
+    body_pixels.fill(OFF)
+
+if ( not 'eye_pixels_count' in globals() ):
+    eye_pixels_count = 2
+if ( not 'eye_pixels_brightness' in globals() ):
+    eye_pixels_brightness = 0
+if ( not 'eye_pixels_count' in globals() ):
+    eye_pixels_count = 0
+if ( not 'use_pixels' in globals() ):
+    eye_use_pixels = False
+    eye_pixels = None
+if ( use_eye_pixels ):
+    if ( not 'eye_pixels_brightness' in globals() ):
+        eye_pixels_brightness = 0.025
+    eye_pixels = neopixel.NeoPixel(eye_pixels_pin, eye_pixels_count, brightness=eye_pixels_brightness,auto_write=True)
+    eye_pixels.fill(OFF)
+
+
+if ( not 'use_display' in globals() ):
+    use_display = False
+
+if ( not 'interval_refresh' in globals() ):
+    interval_refresh = 15
+
+if ( not 'metrics_url' in globals() ):
+    metrics_url = None
+
+if ( ('api_token' in globals()) ):
+    auth_credentials = ":" + api_token
+    auth_token = b64encode(auth_credentials.encode("utf-8")).decode("ascii")
+    headers = {'Authorization': 'Basic ' + auth_token}
+else:
+    headers = {'Authorization': 'None'}
+
+if ( not 'use_ntp' in globals() ):
+    use_ntp = False
+if ( use_ntp ):
+    import adafruit_ntp
+
+def action_Booting():
+    if ( use_eye_pixels ):
+        eye_pixels.fill(RED)
+    elif ( use_body_pixels ):
+        body_pixels.fill(WHITE)
+    time.sleep(2)
+    pass
+
+def action_NoWifi():
+    if (use_eye_pixels):
+        eye_pixels.fill(RED)
+    elif (use_body_pixels):
+        body_pixels.fill(BLUE)
+    time.sleep(2)
+
+def action_HaveWifi():
+    if ( (use_eye_pixels) and (use_body_pixels) ):
+        eye_pixels.fill(WHITE)
+        body_pixels.fill(WHITE)
+    elif (use_eye_pixels):
+        eye_pixels.fill(WHITE)
+    elif (use_body_pixels):
+        body_pixels.fill(PURPLE)
+    time.sleep(2)
+    pass
+
+def action_Unreachable():
+    if ( (use_eye_pixels) and (use_body_pixels) ):
+        eye_pixels.fill(ORANGE)
+    if (use_body_pixels):
+        body_pixels.fill(ORANGE)
+    time.sleep(2)
+
+def action_Pending():
+    if (use_eye_pixels):
+        eye_pixels.fill(WHITE)
+    if (use_body_pixels):
+        body_pixels.fill(YELLOW)
+    time.sleep(2)
+
+def action_Outage():
+    if (use_eye_pixels):
+        eye_pixels.fill(WHITE)
+    if (use_body_pixels):
+        body_pixels.fill(RED)
+    time.sleep(2)
+
+def action_Up():
+    if (use_eye_pixels):
+        eye_pixels.fill(WHITE)
+    if (use_body_pixels):
+        body_pixels.fill(GREEN)
+    time.sleep(2)
+
+if ( use_display ):
+    # This is what I do to set up a 128x64 SSD1306 OLED i2c display to show five lines
+    # of terminal output. It's all very dirty but the point is to have a display for
+    # debugging purposes.
+    
+    # If you have some other display, set it up here.
+
+    import displayio
+    import adafruit_displayio_ssd1306
+    displayio.release_displays()
+
+    i2c = busio.I2C(scl_pin, sda_pin)
+    #i2c = bitbangio.I2C(scl_pin, sda_pin)
+    display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
+    display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
+    display.root_group[0].hidden = False
+
+    try:
+        display.root_group.remove(display.root_group[2])
+        display.root_group.remove(display.root_group[1])
+    except Exception as e:
+        e = None
+
+    # I don't understand what's going on here, I took this from a GitHub issue. The height SHOULD be 64. 
+    # Probably I should just find a font of the correct dimensions but 🤷‍♂️ #yolo
+    supervisor.reset_terminal(display.width, 84)
+
+    # My OLED is yellow/blue, the offset here gets the top pixels of the 2nd row out of the yellow.
+    display.root_group[0].y = 2
+    display.root_group[0].x = 0
+
+# Loop through light actions.
+while ( loop_light_states ):
+    print ("action_Booting()")
+    action_Booting()
+
+    print("action_NoWifi()")
+    action_NoWifi()
+
+    print("action_HaveWifi()")
+    action_HaveWifi()
+
+    print("action_Unreachable()")
+    action_Unreachable()
+
+    print("action_Pending()")
+    action_Pending()
+
+    print("action_Outage()")
+    action_Outage()
+
+    print("action_Up()")
+    action_Up()
+
+    time.sleep(3)
+    if (use_body_pixels):
+        body_pixels.fill(OFF)
+    if (use_eye_pixels):
+        eye_pixels.fill(OFF)
+    time.sleep(3)
+
+action_Booting()
+
+while (True):
+    ssid = os.getenv("CIRCUITPY_WIFI_SSID")
+    password = os.getenv("CIRCUITPY_WIFI_PASSWORD")
+
+    radio = wifi.radio
+    pool = socketpool.SocketPool(radio)
+
+    wifiloopcount = 0
+    while ( (not wifi.radio.ipv4_address) or (not wifi.radio.connected )  ):
+        wifiloopcount += 1
+        if ( wifiloopcount > 24 ):
+            supervisor.reload()
+
+        # if ( use_body_pixels):
+        #     body_pixels.fill(color_NoWifi)
+        action_NoWifi()
+
+        try:
+            print("Connecting to AP...")
+            print("wifi.radio.connected: " + str(wifi.radio.connected))
+            print("wifi.radio.ipv4_address: " + str(wifi.radio.ipv4_address))
+            print("wifiloopcount: " + str(wifiloopcount))
+            wifi.radio.connect(ssid, password)
+            if ( wifiloopcount > 1 ):
+                time.sleep(5)
+        except ConnectionError as e:
+            print("could not connect to AP, retrying: ", e)
+            e = None                
+
+    action_HaveWifi()
+
+    if ( use_ntp ):
+        ntp = adafruit_ntp.NTP(pool, tz_offset=0)
+        timeset = False
+        loopcount = 0
+        while ( (not timeset) and (loopcount < 5) ):
+            try:
+                loopcount += 1
+                rtc.RTC().datetime = ntp.datetime
+                timeset = True
+            except Exception as e:
+                time.sleep(10)
+    
+    loopcount = 0
+    while ( True ):
+        loopcount += 1
+        if ( loopcount > 5 ):
+            supervisor.reload()
+
+        # if ( (loopcount > 2) and (use_body_pixels) ):
+        #     body_pixels.fill(color_Unreachable)
+
+        if (loopcount > 2):
+            action_Unreachable()
+
+        dt = rtc.RTC().datetime
+        seconds=dt.tm_sec
+        minutes=dt.tm_min
+        hour=dt.tm_hour
+        dt = None
+
+        wifiloopcount = 0
+        while ( (not wifi.radio.ipv4_address) or (not wifi.radio.connected ) ):
+            wifiloopcount += 1
+            if ( wifiloopcount > 5 ):
+                supervisor.reload()
+            # if ( use_body_pixels):
+            #     body_pixels.fill(color_NoWifi)
+            action_NoWifi()
+            try:
+                if ( wifiloopcount > 1 ):
+                    time.sleep(5)
+                print("Connecting to AP...")
+                print("wifi.radio.connected: " + str(wifi.radio.connected))
+                print("wifi.radio.ipv4_address: " + str(wifi.radio.ipv4_address))
+                print("wifiloopcount: " + str(wifiloopcount))
+                wifi.radio.connect(ssid, password)
+            except ConnectionError as e:
+                print("could not connect to AP, retrying: ", e)
+                e = None                
+                
+        try:
+            downcount = 0
+            upcount = 0
+            pendingcount = 0
+            maintenancecount = 0 
+            requests = adafruit_requests.Session(pool, None)
+
+            response = requests.get(metrics_url, headers=headers, timeout=interval_timeout)
+            respcode = response.status_code
+            if ( respcode != 200 ):
+                print( "Time:       " + f'{hour:02d} {minutes:02d} {seconds:02d}' )
+                print( "Status: " + f'{respcode:>12}' )
+                print( "Loopcount: " + f'{loopcount:>9}' )
+                print( "Memory free: " + f'{gc.mem_free():>7}' )
+                response.close()
+
+                # if ( (use_body_pixels) and (loopcount > 1) ):
+                #     body_pixels.fill(color_Unreachable)
+                if ( loopcount > 1 ):
+                    action_Unreachable()
+
+            else:
+                resptxt = response.text
+                response.close()
+
+                for line in io.StringIO(resptxt):
+                    ## HELP monitor_status Monitor Status (1 = UP, 0= DOWN, 2= PENDING, 3= MAINTENANCE)
+                    ## monitor_status{monitor_name="name",monitor_type="http",monitor_url="http://mynameisurl",monitor_hostname="null",monitor_port="null"} 1
+                    res = reobj.match(line)
+                    if (res):
+                        if res.group(1) == '0':
+                            downcount += 1
+                        elif res.group(1) == '1':
+                            upcount += 1
+                        elif res.group(1) == '2':
+                            pendingcount += 1
+                        elif res.group(1) == '3':
+                            maintenancecount += 1
+                        else:
+                            # print(res.group(1))
+                            pass
+
+                print( "" )
+                #print( f'{hour:02d} {minutes:02d} {seconds:02d}' + "  " + str(loopcount)  + "  " + f'{gc.mem_free():>7}')
+                print( "Loopcount: " + f'{loopcount:>9}' )
+                print( "Memory free: " + f'{gc.mem_free():>7}' )
+                
+                print( f'{hour:02d} {minutes:02d} {seconds:02d}' + "           " + str(loopcount))
+                print( "Up: " + str(upcount) )
+                print( "Down: " + str(downcount) )
+                print( "Pending: " + str(pendingcount) )
+                print( "Maintenance: " + str(maintenancecount) )
+
+                loopcount = 0
+
+                if ( downcount > 0 ):
+                    # if (use_body_pixels):
+                    #     body_pixels.fill(color_Down)
+                    action_Outage()
+                elif ( pendingcount > 0 ):
+                    # if (use_body_pixels):
+                    #     body_pixels.fill(color_Pending)
+                    action_Pending()
+                else:
+                    # if (use_body_pixels):
+                    #     body_pixels.fill(color_Up)
+                    action_Up()
+       
+        except Exception as e:
+            print( f'{hour:02d} {minutes:02d} {seconds:02d}' + "   " + str(loopcount)  + " " + f'{gc.mem_free():>7}')
+            print(e)
+            e = None
+
+            try: 
+                response.close()
+            except Exception as ex:
+                ex = None
+
+            action_Unreachable()
+
+            response = None
+            requests = None
+
+            # if ( loopcount > 5 ):
+            #     sys.exit()
+            
+        resptxt = None
+        response = None
+        requests = None
+        dt = None
+        downcount = None
+        upcount = None
+        pendingcount = None
+        maintenancecount = None 
+
+        gc.collect()
+        time.sleep(interval_refresh)
+        
